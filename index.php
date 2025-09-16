a144aedc71647bb0f79332aa776d83252ea4839b
Paul Aljon Locsin <151537893+PaulAljon2032@users.noreply.github.com>
2025-09-16 18:09:17 +0800

    add cdn bootstrap for online bootstrap

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="bootstrap-5.3.7-dist/css/bootstrap.min.css">
         <title>Register</title>
     </head>
